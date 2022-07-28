6080de097fa49d406f0d69f4e99ee773a9086209
zora dong <zoradongccc@gmail.com>
2022-07-28 14:26:45 +0800

    add index,show

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'tasks#index'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  get 'tasks/create'
+  get 'tasks/edit'
+  get 'tasks/update'
+  get 'tasks/destroy'
 end
